555951a632f70d7d913d051e5654c063ae5ba518
Vijay Ramakrishnan <vramak@microsoft.com>
2022-05-04 20:10:39 -0700

    Function app changes to trigger message

--- ReminderScheduler/Function1.cs
+++ ReminderScheduler/Function1.cs
@@ -1,16 +1,30 @@
 using System;
+using System.Net.Http;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace ReminderScheduler
 {
-    public class Function1
+    public class ReminderScheduler
     {
-        [FunctionName("Function1")]
-        public void Run([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, ILogger log)
+        private IConfiguration _configuration;
+
+        public ReminderScheduler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [FunctionName("ReminderScheduler")]
+        public void Run([TimerTrigger("* * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            string api = _configuration["API"];
+            if (!string.IsNullOrEmpty(api))
+            {
+                HttpClient client = new HttpClient();
+                client.GetAsync(@"{api}/Reminder");
+            }
         }
     }
 }
