51b77a80daac8f3aecb675b77486d4e7d63c8e84
Vijay Ramakrishnan <vramak@microsoft.com>
2022-05-05 08:00:36 -0700

    Setup SignalR

--- ReminderAlerter/Program.cs
+++ ReminderAlerter/Program.cs
@@ -1,16 +1,40 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
+
+builder.Services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+{
+    builder.WithOrigins("https://localhost:7292").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
+app.UseResponseCompression();
+app.UseCors("ApiCorsPolicy");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
-
-app.MapGet("/hello", () => "Hello, World!");
+app.MapHub<ReminderHub>("/reminder");
+await using var scope = app.Services?.GetService<IServiceScopeFactory>()?.CreateAsyncScope();
+var context = scope?.ServiceProvider.GetRequiredService<IHubContext<ReminderHub>>();
+app.MapGet("/broadcast", async (string reminder) =>
+{
+    await context!.Clients.All.SendAsync("ReceiveMessage", $"{DateTime.Now.ToString("MMMM dd H:mm:ss tt")}: {reminder}");
+});
 
 app.Run();
+
+public class ReminderHub : Hub { }
